e56c599cb5ecd3791fd774f04c176fecde3531f4
sandix34 <sandrinezocca@gmail.com>
2022-03-15 11:55:57 +0100

    MongoDB database connection

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require("dotenv").config();
+require("./config/database").connect();
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
