a94bc376f3dd4e127a5196a52c8283692004bc0e
sandix34 <sandrinezocca@gmail.com>
2022-03-15 11:17:24 +0100

    Connecting Angular client application and Express server

--- server/app.js
+++ server/app.js
@@ -10,10 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+app.use(express.static(path.join(__dirname, '../public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
