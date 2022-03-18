42f6c25cc8852c1f2d763ff8ee5a432abd579c64
sandix34 <sandrinezocca@gmail.com>
2022-03-18 16:04:41 +0100

    create register form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
