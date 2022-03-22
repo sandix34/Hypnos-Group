c2b88e477c58694752c08cf5e44810dfe88002df
sandix34 <sandrinezocca@gmail.com>
2022-03-22 10:19:56 +0100

    create the middleware that checks if the user contains the required role

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -17,8 +17,61 @@ verifyToken = (req, res, next) => {
         next();
     });
 };
-
+isAdmin = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+        Role.find(
+            {
+                _id: { $in: user.roles }
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "admin") {
+                        next();
+                        return;
+                    }
+                }
+                res.status(403).send({ message: "Require Admin Role!" });
+            }
+        );
+    });
+};
+isManager = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+        Role.find(
+            {
+                _id: { $in: user.roles }
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "manager") {
+                        next();
+                        return;
+                    }
+                }
+                res.status(403).send({ message: "Require Manager Role!" });
+            }
+        );
+    });
+};
 const authJwt = {
     verifyToken,
+    isAdmin,
+    isManager
 };
 module.exports = authJwt;
