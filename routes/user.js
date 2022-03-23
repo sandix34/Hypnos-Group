ff7d4c64eaad2ad014e2b40244d265299948792f
sandix34 <sandrinezocca@gmail.com>
2022-03-23 16:33:10 +0100

    Add component home to connect public content service

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,6 @@ const router = require('express').Router();
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
 
-module.exports = function(app) {
-    router.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
     router.get("/all", controller.allAccess);
     router.get("/user", [authJwt.verifyToken], controller.userBoard);
     router.get(
@@ -22,6 +14,5 @@ module.exports = function(app) {
         [authJwt.verifyToken, authJwt.isAdmin],
         controller.adminBoard
     );
-};
 
 module.exports = router;
