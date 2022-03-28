9f4b27b78355cdd33e5f70d9803a3ef280e8cc80
sandix34 <sandrinezocca@gmail.com>
2022-03-28 14:08:42 +0200

    method to create a hotel and save in the database

--- controllers/hotel.controller.js
+++ controllers/hotel.controller.js
@@ -3,7 +3,31 @@ const Hotel = db.hotel;
 
 // Create and Save a new Hotel
 exports.create = (req, res) => {
-
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+    // Create a Hotel
+    const hotel = new Hotel({
+        title: req.body.title,
+        city: req.body.city,
+        address: req.body.address,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    });
+    // Save Hotel in the database
+    hotel
+        .save(hotel)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Hotel."
+            });
+        });
 };
 
 // Retrieve all Hotels from the database.
