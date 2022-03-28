b0cd360c98c55c1f6a67bdd573658723dbf374e9
sandix34 <sandrinezocca@gmail.com>
2022-03-28 14:01:00 +0200

    prepare the controllers

---
+++ controllers/hotel.controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Hotel = db.hotel;
+
+// Create and Save a new Hotel
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Hotels from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Hotel with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Hotel by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Hotel with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Hotel from the database.
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Hotel
+exports.findAllPublished = (req, res) => {
+
+};
