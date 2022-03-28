0c5dda6a2c6b84f5a1278632dc9d09ad119766ee
sandix34 <sandrinezocca@gmail.com>
2022-03-28 17:38:14 +0200

    create the form to add a hotel in the database

--- client/src/app/add-hotel/add-hotel.component.ts
+++ client/src/app/add-hotel/add-hotel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HotelService} from "../_services/hotel.service";
 
 @Component({
   selector: 'app-add-hotel',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddHotelComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup = this.fb.group({
+    name: [null, Validators.required],
+    city: [null, Validators.required],
+    address: [null, Validators.required],
+    description: [null, Validators.required]
+  })
+
+  isSuccessful = false;
+
+  constructor(private fb: FormBuilder, private hotelService: HotelService) { }
 
   ngOnInit(): void {
   }
 
+  public error!: string;
+
+  public submit() {
+    if (this.form.valid) {
+      this.hotelService.create(this.form.getRawValue()). subscribe({
+        next: (data) => {
+          console.log(data);
+          this.isSuccessful = true;
+        },
+        error: (err) => {
+          this.error = err?.error || 'An error has occurred';
+        }
+      })
+    }
+  }
 }
